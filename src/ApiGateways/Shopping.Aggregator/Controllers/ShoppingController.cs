2e0d0cb2b62d1b2d358b419664c3570cdf855cc7
bigprice <bigangelito@gmail.com>
2022-04-22 12:46:10 -0500

    adding ocelot api gateway and aggregator pattern

---
+++ src/ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public ShoppingController(ICatalogService catalogService, IBasketService basketService, IOrderService orderService)
+        {
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
+
+        [HttpGet("{userName}", Name = "GetShoopping")]
+        [ProducesResponseType(typeof(ShoppingModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+        {
+            // Get basket with username
+            var basket = await _basketService.GetBasket(userName);
+
+            // Iterate basket items and consume prducts with basket item productid member
+            foreach (var item in basket.Items)
+            {
+                var product = await _catalogService.GetCatalog(item.ProductId);
+
+                // Set additional product fields onto basket item
+                item.ProductName = product.Name;
+                item.Category = product.Category;
+                item.Summary = product.Summary;
+                item.Description = product.Description;
+                item.ImageFile = product.ImageFile;
+            }
+
+            // Map product related members into basket item dto with extended column
+
+
+            // Consume ordering microservices in order to retrieve order list
+            var orders = await _orderService.GetOrdersByUserName(userName);
+
+            // return root ShoppingModel dto classs which including all responses
+            var shoppingModel = new ShoppingModel
+            {
+                UserName = userName,
+                 BasketWithProducts = basket,
+                 Orders = orders
+            };
+
+            return Ok(shoppingModel);
+
+        }
+
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _client = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var responce = await _client.GetAsync($"/api/v1/Basket/{userName}");
+            return await responce.ReadContentAs<BasketModel>();
+        }
+    }
+}

56f28bbad222f34fcbd8a8a7d957e25b9004e7f6
bigprice <bigangelito@gmail.com>
2022-05-27 16:17:09 -0500

    Refactor de funcionalidad

---
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _client = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrderByUserName(string userName)
+        {            
+            var response = await _client.GetAsync($"/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
